a4421e772170d61c393dd0a57de2d3f6e8197cb8
Asiholanpasraibu <160842986+Asiholanpasraibu@users.noreply.github.com>
2025-11-03 00:57:26 +0700

    Add files via upload

---
+++ dart-test-master/bin/Holan_1.dart
@@ -0,0 +1,5 @@
+import 'package:holan_1/holan_1.dart' as holan_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${holan_1.calculate()}!');
+}
---
+++ dart-test-master/test/Holan_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:holan_1/holan_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
